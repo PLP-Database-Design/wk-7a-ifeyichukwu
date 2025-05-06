4640bb9a3fbf2604d45ed4d2d438c511ccac1804
ifeyichukwu <124167353+ifeyichukwu@users.noreply.github.com>
2025-05-06 18:46:37 +0100

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -38,3 +38,12 @@ CREATE TABLE Products (
     Quantity INT,
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
+
+INSERT INTO Products (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
